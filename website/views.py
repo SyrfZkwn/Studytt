03a022b78de919962a6881121d400db23203290e
Syarif Zakwan <zakwankhabir@gmail.com>
2025-04-13 20:41:09 +0800

    feat: added nav-bar and logo

--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ views = Blueprint('view', __name__)
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/post')
+def post():
+    return render_template("post.html")

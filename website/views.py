26a8e5e6f0715b775cd1d989d029a58892bb14e1
Syarif Zakwan <zakwankhabir@gmail.com>
2025-04-18 01:05:23 +0800

    feat: website template done, except for profile icon and settings

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 views = Blueprint('view', __name__)
 
-@views.route('/')
+@views.route('/home')
 def home():
     return render_template("home.html")
 

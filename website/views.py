ed4b1f1fad6b4ef14460084491b0afa66c56834b
Syarif Zakwan <zakwankhabir@gmail.com>
2025-04-21 01:07:10 +0800

    fixed post feature

--- website/views.py
+++ website/views.py
@@ -1,18 +1,16 @@
 from flask import Blueprint, render_template, current_app
 from flask_login import login_required, current_user
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField, StringField, DateField, BooleanField
+from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
 
-class UploadFileForm(FlaskForm):
-    subject = StringField("subject", validators=[InputRequired()])
-    chapter = StringField("chapter", validators=[InputRequired()])
-    file = FileField("file", validators=[InputRequired()])
-    submit = SubmitField("POST")
-
 views = Blueprint('views', __name__)
+
+class UploadFileForm(FlaskForm):
+    file = FileField("File", validators=[InputRequired()])
+    submit = SubmitField("Upload")
 
 @views.route('/home')
 @login_required
@@ -20,6 +18,7 @@ def home():
     return render_template("home.html", user=current_user)
 
 @views.route('/post', methods=['GET', 'POST'])
+@login_required
 def post():
     form = UploadFileForm()
     if form.validate_on_submit():
